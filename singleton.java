b58b85b112de53ae4f57e5aac653f17754eb0d88
Muihre <nuckelavee1097@gmail.com>
2018-05-22 08:42:29 +0200

    dodanie zadań

---
+++ singleton.java
@@ -0,0 +1,30 @@
+﻿import java.util.ArrayList;
+
+public class Singleton
+{
+	
+	private static ArrayList<String> list = null;
+	
+	public static void add(String element)
+	{
+		list.add(element);
+	}
+	
+		public static void remove(String element)
+	{
+		list.remove(element);
+	}
+	
+	public static ArrayList<String> get()
+	{
+		return list;
+	}
+	
+	private static Singleton singletonInstance = new Singleton();
+	
+	public static Singleton getInstance()
+	{
+		return singletonInstance;
+	}
+
+}
